38390cf3556c6864175c46341ccec118ef399803
stashymane <me@stashy.dev>
2026-06-26 16:14:39 +0300

    collect jvm version from main libs catalog

--- src/main/kotlin/multiplatform/target/androidLibrary.gradle.kts
+++ src/main/kotlin/multiplatform/target/androidLibrary.gradle.kts
@@ -2,12 +2,10 @@ package multiplatform.target
 
 import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryTarget
 import getIntVersionOrThrow
-import getOrThrow
 import getVersionCatalog
 import getVersionOrThrow
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import toIntOrElseThrow
 
 plugins {
     id("multiplatform.base")
@@ -15,6 +13,7 @@ plugins {
 
 configure<KotlinMultiplatformExtension> {
     this.extensions.configure<KotlinMultiplatformAndroidLibraryTarget>("android") {
+        val libs = getVersionCatalog("libs")
         val androidLibs = getVersionCatalog("androidLibs")
 
         namespace = project.group.toString()
@@ -24,7 +23,7 @@ configure<KotlinMultiplatformExtension> {
         withJava()
 
         compilerOptions {
-            val jvmVersion = androidLibs.getVersionOrThrow("jvm").requiredVersion
+            val jvmVersion = libs.getVersionOrThrow("jvm").requiredVersion
             jvmTarget = JvmTarget.fromTarget(jvmVersion)
         }
     }
