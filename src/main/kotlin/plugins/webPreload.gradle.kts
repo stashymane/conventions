1b2b716ae0254f633ca37e19edf3f642aa88d951
stashymane <me@stashy.dev>
2026-09-18 14:15:01 +0300

    disable all preload detection by default

--- src/main/kotlin/plugins/webPreload.gradle.kts
+++ src/main/kotlin/plugins/webPreload.gradle.kts
@@ -5,8 +5,8 @@ import plugins.preload.configureWebPreload
 
 val webPreload = extensions.create<WebPreloadExtension>("webPreload").apply {
     distribution.enabled.convention(true)
-    distribution.wasm.convention(true)
-    distribution.mjs.convention(true)
+    distribution.wasm.convention(false)
+    distribution.mjs.convention(false)
     distribution.js.convention(false)
 }
 
